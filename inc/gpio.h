e4d3ce71d4283e426f74d34a38038f7dda93364a
DanteMendoza <31194413+DanteMendoza@users.noreply.github.com>
2024-08-31 00:26:39 -0300

    tp4_v3
    comentarios de Doxygen en gpio.h

--- inc/gpio.h
+++ inc/gpio.h
@@ -1,16 +1,50 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-// void gpioSetDirection(uint8_t puerto, uint8_t bit, bool salida);
-// void gpioSetOutput(uint8_t puerto, uint8_t bit, bool estado);
-// bool gpioGetInput(uint8_t puerto, uint8_t bit);
-
+/**
+ * @brief Defino el tipo de puntero a una estructura GPIO.
+ *
+ * Este tipo se usa para manejar instancias de GPIO en el sistema.
+ */
 typedef struct gpio_s * gpio_t;
 
+/**
+ * @brief Creo una nueva instancia de un objeto GPIO.
+ *
+ * Esta función inicializa un nuevo objeto GPIO en el puerto y bit especificados.
+ *
+ * @param puerto El número del puerto del GPIO.
+ * @param bit El número del bit en el puerto del GPIO.
+ * @return Un puntero al nuevo objeto GPIO creado.
+ */
 gpio_t gpioCreate(uint8_t puerto, uint8_t bit);
 
+/**
+ * @brief Establece el valor de salida del GPIO.
+ *
+ * Esta función configura el estado de salida del GPIO a verdadero (activo) o falso (inactivo).
+ *
+ * @param gpio El puntero al objeto GPIO.
+ * @param output El estado de salida a configurar (true para activo, false para inactivo).
+ */
 void gpioSetOutput(gpio_t gpio, bool output);
 
+/**
+ * @brief Establece el estado del GPIO.
+ *
+ * Esta función configura el estado del GPIO a verdadero (activo) o falso (inactivo).
+ *
+ * @param gpio El puntero al objeto GPIO.
+ * @param state El estado a configurar (true para activo, false para inactivo).
+ */
 void gpioSetState(gpio_t gpio, bool state);
 
+/**
+ * @brief Obtiene el estado actual del GPIO.
+ *
+ * Esta función devuelve el estado actual del GPIO (verdadero para activo, falso para inactivo).
+ *
+ * @param gpio El puntero al objeto GPIO.
+ * @return El estado actual del GPIO (true si está activo, false si está inactivo).
+ */
 bool gpioGetState(gpio_t gpio);
